cc6692ea0578429d49cfb136b2b4660c28255bd1
Jeremy uzan <jeremyuzan@hotmail.fr>
2021-01-21 04:34:56 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,81 +1,28 @@
 
 
-//spectrogramme 
+//spectrogramme
 var started = null;
-window.addEventListener('click', () => {
-    if (started) return;
-    started = true;
-    initialize();
-})
+window.addEventListener("click", () => {
+  if (started) return;
+  started = true;
+  initialize();
+});
 
 function initialize() {
-    document.body.querySelector('h1').remove();
-    const CVS =document.body.querySelector('canvas');
-    const CTX = CVS.getContext('2d');
-    const W = CVS.width = window.innerWidth;
-    const H = CVS.height = window.innerHeight;
+  document.body.querySelector("h1").remove();
+  const CVS = document.body.querySelector("canvas");
+  const CTX = CVS.getContext("2d");
+  const W = (CVS.width = window.innerWidth);
+  const H = (CVS.height = window.innerHeight);
 
-    const ACTX = new AudioContext();
-    const ANALYSER = ACTX.createAnalyser();
+  // THIS IS REALLY IMPORTANT. we want to use the same context as tone.js
+  const ACTX = Tone.context;
+  const ANALYSER = ACTX.createAnalyser();
 
-    ANALYSER.fftSize = 4096;  
-    
-    navigator.mediaDevices
-    .getUserMedia({ audio: true })
-    .then(process);
+  ANALYSER.fftSize = 4096;
 
-    function process(stream) {
-        const SOURCE = ACTX.createMediaStreamSource(stream);
-        SOURCE.connect(ANALYSER);
-        const DATA = new Uint8Array(ANALYSER.frequencyBinCount);
-        const LEN = DATA.length;
-        const h = H / LEN;
-        const x = W - 1;
-        CTX.fillStyle = 'hsl(280, 100%, 10%)';
-        CTX.fillRect(0, 0, W, H);
-
-        loop();
-
-        function loop() {
-        window.requestAnimationFrame(loop);
-        let imgData = CTX.getImageData(1, 0, W - 1, H);
-        CTX.fillRect(0, 0, W, H);
-        CTX.putImageData(imgData, 0, 0);
-        ANALYSER.getByteFrequencyData(DATA);
-        for (let i = 0; i < LEN; i++) {
-            let rat = DATA[i] / 255;
-            let hue = Math.round((rat * 120) + 280 % 360);
-            let sat = '100%';
-            let lit = 10 + (70 * rat) + '%';
-            CTX.beginPath();
-            CTX.strokeStyle = `hsl(${hue}, ${sat}, ${lit})`;
-            CTX.moveTo(x, H - (i * h));
-            CTX.lineTo(x, H - (i * h + h));
-            CTX.stroke();
-        }
-        }
-    }
-    }
-
-
-    let rot = 0;
-    tick();
-    
-    function tick() {
-      requestAnimationFrame(tick);
-      rot += 0.02;
-      document.body.style.setProperty("--rot", `${rot}deg`);
-    }
-
-
-
-
-
-
-
-
-
-
-
-    
-
+  const synth = new Tone.Synth();
+  // Set the tone to sine
+  synth.oscillator.type = "sine";
+  // connect it to the master output (your speakers)
+  // (this used to be called toMaster() but is now called toDestination() in Tone.js)
